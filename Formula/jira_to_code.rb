51594d59f7a5f476f3160ca3eb8a059a38948984
imaresss <pulkitkedia1197@gmail.com>
2026-03-13 18:06:10 +0530

    github actions

---
+++ Formula/jira_to_code.rb
@@ -0,0 +1,22 @@
+class JiraToCode < Formula
+  desc "jira_to_code.sh is the main script which connects to jira cli,asks user for the code repository, branch, ai agent to be used and then redirects it to either codex.sh or cursor.sh . Codex.sh connected to codex cli , cursor.sh connects to cursor cli"
+  homepage "https://github.com/imaresss/jira_to_code"
+  # The URL you copied from your GitHub release
+  url "https://github.com/imaresss/jira_to_code/archive/refs/tags/v1.0.1.tar.gz"
+  # The hash you calculated in Step 1
+  sha256 "0ff335c5ff97b4521597882fa112d87164bf44a62eaef8a92834cf842fc51b26"
+  license "MIT" # Update this to match your actual license
+
+  def install
+    # This installs the scripts into the Homebrew binary directory.
+    # The right side of the hash allows you to drop the '.sh' extension for the user.
+    bin.install "jira_to_code" => "jira_to_code"
+    bin.install "cursor.sh"
+    bin.install "codex.sh"
+  end
+
+  test do
+    # A simple test to ensure the scripts were installed successfully
+    system "#{bin}/jira_to_code", "--version" 
+  end
+end
